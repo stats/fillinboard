91b394d240ac745f8e7d9472c0562aa57e8dcf74
Dan Curran <codetown@gmail.com>
2020-04-20 14:52:40 -0400

    Usability fixes

--- src/index.ts
+++ src/index.ts
@@ -65,11 +65,12 @@ io.on('connection', (socket) => {
 
   socket.emit('units', units);
 
-  socket.on('name message', (msg) => {
-    console.log('Client Provided Name: ' + msg);
+  socket.on('submit-name', (msg) => {
+    console.log('Client Submitted Name: ' + msg);
     clients[socket.id].name = msg;
     socket.emit('signed-in', clients[socket.id].name);
     socket.emit('units', units);
+    io.sockets.emit('users', clientsToStringArray(clients));
     socket.broadcast.emit('joined', clients[socket.id].name);
   });
 
@@ -108,6 +109,7 @@ io.on('connection', (socket) => {
   })
 
   socket.on('disconnect', () => {
+    delete clients[socket.id];
     console.log('user disconnected');
   });
 })
@@ -115,6 +117,17 @@ io.on('connection', (socket) => {
 http.listen(4199, () => {
   console.log('listening on *:4199');
 })
+
+function clientsToStringArray(clients) {
+  let a = [];
+  for(let client in clients) {
+    let c = clients[client];
+    if(c.name != null) {
+      a.push(c.name);
+    }
+  }
+  return a;
+}
 
 function resetUnits() {
   units = [{"id":"E1","top":1022.0039,"left":665.0039,"color":"black"},{"id":"E2","top":680.0039,"left":549.0039,"color":"black"},{"id":"E3","top":768.0039,"left":221.0039,"color":"black"},{"id":"E5","top":855.0039,"left":339.0039,"color":"black"},{"id":"E7","top":1146.0039,"left":351.0039,"color":"black"},{"id":"E8","top":1263.0039,"left":708.0039,"color":"black"},{"id":"E9","top":770.0039,"left":828.0039,"color":"black"},{"id":"E10","top":576.0039,"left":341.0039,"color":"black"},{"id":"E12","top":920.0039,"left":965.0039,"color":"black"},{"id":"E13","top":1010.0039,"left":431.0039,"color":"black"},{"id":"E16","top":738.0039,"left":663.0039,"color":"black"},{"id":"E17","top":863,"left":613,"color":"black"},{"id":"E19","top":197.0039,"left":503.0039,"color":"black"},{"id":"T2","top":639.0039,"left":342.0039,"color":"red"},{"id":"T3","top":1206,"left":512,"color":"red"},{"id":"T4","top":1037.0039,"left":887.0039,"color":"red"},{"id":"T5","top":1003,"left":241,"color":"red"},{"id":"T6","top":791,"left":671,"color":"red"},{"id":"T10","top":945,"left":429,"color":"red"},{"id":"R11","top":927,"left":647,"color":"blue"}];
