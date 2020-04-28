761b58db9245afb854330af6e21405a1f0393568
Dan Curran <codetown@gmail.com>
2020-04-28 12:30:41 -0400

    Resize image, make units squares, double click to put on call, allow unit to show having a tool. Make add and remove easier.

--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,12 @@ app.get('/', (req, res) => {
   res.send('<h1>Fillin Board</h1>');
 });
 
+app.get('/tool.png', (req, res) => {
+  res.sendFile(__dirname + '/client/tool.png');
+});
+
 app.get('/fillin-map.png', (req, res) => {
-  res.sendFile(__dirname + '/client/fillin-map-v2.png');
+  res.sendFile(__dirname + '/client/fillin-map-v3.png');
 });
 
 app.get('/DragDropTouch.js', (req, res) => {
@@ -87,6 +91,8 @@ io.on('connection', (socket) => {
 
   socket.on('add-unit', (msg) => {
     console.log('Add unit', msg);
+    let unit = units.find(u => u.id === msg.id);
+    if(unit != null) return;
     if(msg == null || msg.id == null || msg.id == '') {
       socket.emit('add-error', 'Cannot add a blank unit');
       return;
@@ -106,7 +112,18 @@ io.on('connection', (socket) => {
     resetUnits();
     io.sockets.emit('message', clients[socket.id].name + " has reset all units.");
     io.sockets.emit('units', units);
-  })
+  });
+
+  socket.on('toggle-on-call', (msg) => {
+    let unit = units.find(unit => unit.id === msg.id);
+    if(!unit) return;
+    if(unit.onCall == null || unit.onCall == false) {
+      unit.onCall = true;
+    } else {
+      unit.onCall = false;
+    }
+    io.sockets.emit('units', units);
+  });
 
   socket.on('disconnect', () => {
     delete clients[socket.id];
@@ -130,5 +147,24 @@ function clientsToStringArray(clients) {
 }
 
 function resetUnits() {
-  units = [{"id":"E1","top":1022.0039,"left":665.0039,"color":"black"},{"id":"E2","top":680.0039,"left":549.0039,"color":"black"},{"id":"E3","top":768.0039,"left":221.0039,"color":"black"},{"id":"E5","top":855.0039,"left":339.0039,"color":"black"},{"id":"E7","top":1146.0039,"left":351.0039,"color":"black"},{"id":"E8","top":1263.0039,"left":708.0039,"color":"black"},{"id":"E9","top":770.0039,"left":828.0039,"color":"black"},{"id":"E10","top":576.0039,"left":341.0039,"color":"black"},{"id":"E12","top":920.0039,"left":965.0039,"color":"black"},{"id":"E13","top":1010.0039,"left":431.0039,"color":"black"},{"id":"E16","top":738.0039,"left":663.0039,"color":"black"},{"id":"E17","top":863,"left":613,"color":"black"},{"id":"E19","top":197.0039,"left":503.0039,"color":"black"},{"id":"T2","top":639.0039,"left":342.0039,"color":"red"},{"id":"T3","top":1206,"left":512,"color":"red"},{"id":"T4","top":1037.0039,"left":887.0039,"color":"red"},{"id":"T5","top":1003,"left":241,"color":"red"},{"id":"T6","top":791,"left":671,"color":"red"},{"id":"T10","top":945,"left":429,"color":"red"},{"id":"R11","top":927,"left":647,"color":"blue"}];
+  units = [ { id: 'E1', top: 478, left: 299, color: 'black', hasTool: true },
+  { id: 'E2', top: 311, left: 242, color: 'black' },
+  { id: 'E3', top: 351, left: 99, color: 'black' },
+  { id: 'E5', top: 395, left: 151, color: 'black' },
+  { id: 'E7', top: 533, left: 156, color: 'black' },
+  { id: 'E8', top: 582, left: 327, color: 'black' },
+  { id: 'E9', top: 354, left: 377, color: 'black'},
+  { id: 'E10', top: 264, left: 151, color: 'black', hasTool: true },
+  { id: 'E12', top: 425, left: 440, color: 'black'},
+  { id: 'E13', top: 443, left: 191, color: 'black' },
+  { id: 'E16', top: 335, left: 301, color: 'black' },
+  { id: 'E17', top: 403, left: 278, color: 'black' },
+  { id: 'E19', top: 88, left: 223, color: 'black' },
+  { id: 'T2', top: 295, left: 150, color: 'red' },
+  { id: 'T3', top: 552, left: 230, color: 'red' },
+  { id: 'T4', top: 480, left: 401, color: 'red' },
+  { id: 'T5', top: 459, left: 107, color: 'red' },
+  { id: 'T6', top: 359, left: 301, color: 'red' },
+  { id: 'T10', top: 474, left: 189, color: 'red' },
+  { id: 'R11', top: 427, left: 278, color: 'blue', hasTool: true } ];
 }
